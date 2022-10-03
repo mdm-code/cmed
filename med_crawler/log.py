13355a6119146629086b3a6c8d942ded74d87d76
mdm-code <code.madamczyk@gmail.com>
2022-10-03 22:52:20 +0200

    [FEAT] Implemented basic logger class
    This class lets you log messages to a file
    to keep track of changes.

---
+++ med_crawler/log.py
@@ -0,0 +1,41 @@
+"""Logging components."""
+
+# Standard library imports
+import enum
+from typing import Generator, TextIO, Protocol
+
+
+class LoggingError(Exception):
+    ...
+
+
+class Level(str, enum.Enum):
+    warning = WARN = "WARNING"
+    error = ERROR = "ERROR"
+
+
+class Logger(Protocol):
+    def __init__(self, out: TextIO) -> None:
+        raise NotImplementedError
+
+    def log(self, msg: str, lvl: Level) -> None:
+        raise NotImplementedError
+
+
+class CrawlerLogger:
+    def __init__(self, out: TextIO) -> None:
+        self.out = out
+        self.counter = count(0, 1)
+
+    def log(self, msg: str, lvl: Level) -> None:
+        match lvl:
+            case lvl.error | lvl.warning:
+                self.out.write(f"{next(self.counter)} {format(lvl)}: {msg}\n")
+            case _:
+                raise LoggingError(f"{str(lvl)} not supported")
+
+
+def count(start: int = 0, step: int = 1) -> Generator[int, None, None]:
+    while True:
+        yield start
+        start += step
