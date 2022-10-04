bbdaec587d4f71e1e139b2c90c712c521035ff6b
mdm-code <code.madamczyk@gmail.com>
2022-10-04 23:06:46 +0200

    [FEAT] Added datetime timestamp to log messages
    This makes it easier to tell when a given page
    was crawled.

--- med_crawler/log.py
+++ med_crawler/log.py
@@ -1,8 +1,9 @@
 """Logging components."""
 
 # Standard library imports
+import datetime
 import enum
-from typing import Generator, TextIO, Protocol
+from typing import Generator, TextIO, Protocol, ClassVar
 
 
 class LoggingError(Exception):
@@ -16,22 +17,30 @@ class Level(str, enum.Enum):
 
 
 class Logger(Protocol):
-    def __init__(self, out: TextIO) -> None:
-        raise NotImplementedError
-
     def log(self, msg: str, lvl: Level) -> None:
         raise NotImplementedError
 
 
 class CrawlerLogger:
-    def __init__(self, out: TextIO) -> None:
+    date_fmt: ClassVar[str] = "%y-%m-%d %H:%M:%S.%f"
+
+    def __init__(self, out: TextIO, include_date: bool = True) -> None:
         self.out = out
+        self.include_date = include_date
         self.counter = count(0, 1)
 
     def log(self, msg: str, lvl: Level) -> None:
         match lvl:
             case lvl.ok | lvl.error | lvl.warning:
-                self.out.write(f"{next(self.counter)} {format(lvl)}: {msg}\n")
+                if self.include_date:
+                    time = datetime.datetime.now().strftime(self.date_fmt)
+                    self.out.write(
+                        f"{next(self.counter)}::{format(lvl)}::{time}::{msg}\n"
+                    )
+                else:
+                    self.out.write(
+                        f"{next(self.counter)}::{format(lvl)}::{msg}\n"
+                    )
                 self.out.flush()
             case _:
                 raise LoggingError(f"{str(lvl)} not supported")
--- tests/test_log.py
+++ tests/test_log.py
@@ -10,12 +10,12 @@ from io import StringIO
 def test_crawler_logger() -> None:
     out = StringIO()
     with contextlib.redirect_stdout(out):
-        l = log.CrawlerLogger(sys.stdout)
+        l = log.CrawlerLogger(sys.stdout, False)
         l.log("this is a warning", log.Level.WARN)
         l.log("this is an error!", log.Level.ERROR)
     out.seek(0)
     assert out.readlines() == [
-        "0 WARNING: this is a warning\n",
-        "1 ERROR: this is an error!\n",
+        "0::WARNING::this is a warning\n",
+        "1::ERROR::this is an error!\n",
     ]
 
